3297d6fd6021ccec4c215e1891a4de46b22c5a3d
gsambou <ghislain.sambou@gmail.com>
2022-07-07 12:42:50 +0200

    refactoring

--- index.mjs
+++ index.mjs
@@ -1,13 +1,3 @@
-import { listenBtnClick, closeModalFunc, dataFormProcess, editNav } from './lib/index.mjs';
+import gameOn from './lib/index.mjs';
 
-// listen btn click and launch modal event
-listenBtnClick();
-
-// close modal
-closeModalFunc();
-
-// Process data get with form
-dataFormProcess();
-
-// launch edit nav
-editNav();
+gameOn();
